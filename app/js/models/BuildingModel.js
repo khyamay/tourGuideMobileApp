2b9661e3afce7c0c4407555e71ff06990533ecb2
Khemraj Thapa <khyamay@gmail.com>
2014-02-07 14:10:41 +0000

    added templates, models and routes

---
+++ app/js/models/BuildingModel.js
@@ -0,0 +1,29 @@
+app.models.Building = Backbone.Model.Extend({
+	
+	initailize: function(){
+		this.building = new app.models.BuildingCollection();
+		this.building.parent = this;
+	},
+
+	sync: function(method, model, options){
+		if(method === "read"){
+			app.adapters.building.findById(parseInt(this.id)).done(function(data){
+				options.success(data);
+			});
+		}
+	}
+
+
+});
+
+app.models.BuildingCollection = Backbone.Collection.extend({
+	model: app.models.Building,
+
+	sync: function(method, model, options){
+		if(method === "read"){
+			app.adapters.building.findByName(options.data.name).done(function(data){
+				options.success(data);
+			});
+		}
+	}
+});
